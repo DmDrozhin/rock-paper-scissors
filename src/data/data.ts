b5054b2e0b4dbeee083bd9d6cff6f9a95837d274
Твоё имя < email  GitHub>
2026-01-19 20:54:57 +0200

    feat: add initial data structure for Rock Paper Scissors game

---
+++ src/data/data.ts
@@ -0,0 +1,18 @@
+import { getImagePath } from '../utils/utils.ts';
+export const META = {
+  name: 'Rock Paper Scissors'
+};
+export const HEADER = {
+  button: 'BOOK A CALL'
+};
+export const RPS = {
+  title: 'ROCK PAPER SCISSORS',
+  description:
+    '<div class="description">Practical email marketing services. Clear <span class="highlight">tasks</span>. Working <span class="highlight">solutions</span>.</div>',
+  button: {
+    title: 'Discuss my task',
+    img: getImagePath('cardboard_airplane_wt.svg'),
+    img_alt: 'Button icon',
+    img_size: 24
+  }
+};
