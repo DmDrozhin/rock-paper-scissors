05420871dd741f2a90b3a79d25ea9eb01c9ac1eb
Твоё имя < email  GitHub>
2026-01-28 16:01:10 +0200

    feat: add SVG for email tools setup and enhance data structure with new sections

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,4 +1,4 @@
-import { getImagePath } from '@/utils/utils';
+import type { UiIcon } from '@/assets/assets';
 
 export const META = {
   name: 'Rock Paper Scissors'
@@ -15,27 +15,41 @@ export const RPS = {
     aria_label: 'Discuss my task with Rock Paper Scissors team'
   }
 };
-
-export const WHO_WE_ARE = {
+interface Card {
+  title: string;
+  list?: string[];
+  img: UiIcon;
+  img_size: number;
+}
+interface WhoWeAre {
+  title: string;
+  description: string;
+  cards: Card[];
+  remark: string;
+  button: {
+    title: string;
+  };
+}
+export const WHO_WE_ARE: WhoWeAre = {
   title: 'Who We Are',
   description: 'An email marketing agency built around practical solutions.',
   cards: [
     {
       title: 'Content',
       list: ['Email copywriting for campaigns and flows', 'Automatization', 'Promotional campaigns'],
-      img: getImagePath('btn_circle.svg'),
+      img: 'BTN_CIRCLE',
       img_size: 100
     },
     {
       title: 'Optimization',
       list: ['Email and flow reviews', 'Audience segmentation', 'Delivery rate', 'Conversion rate'],
-      img: getImagePath('btn_rectangle.svg'),
+      img: 'BTN_RECTANGLE',
       img_size: 100
     },
     {
       title: 'Setup',
       list: ['Trigger-based emails', 'Email content planning', 'Domain and technical setup'],
-      img: getImagePath('btn_cross.svg'),
+      img: 'BTN_CROSS',
       img_size: 80
     }
   ],
@@ -43,4 +57,76 @@ export const WHO_WE_ARE = {
   button: {
     title: 'Discuss'
   }
+};
+
+interface HowWeWork {
+  title: string;
+  description: string;
+  cards: {
+    title: string;
+    img: UiIcon;
+  }[];
+}
+export const HOW_WE_WORK: HowWeWork = {
+  title: 'How We Work',
+  description:
+    '<div class="assertion">One <span class="accent-text">task</span> — equal <span class="accent-text">solution.</span></div>',
+  cards: [
+    {
+      title: 'Email Review',
+      img: 'EMAIL_REVIEW'
+    },
+    {
+      title: 'Product Context',
+      img: 'PRODUCT_CONTEXT'
+    },
+    {
+      title: 'Email Tools Setup',
+      img: 'EMAIL_TOOLS_SETUP'
+    },
+    {
+      title: 'Basic Competitor Review',
+      img: 'BASIC_COMPETITOR_REVIEW'
+    },
+    {
+      title: 'Email Performance Tracking',
+      img: 'EMAIL_PERFORMANCE_TRACKING'
+    },
+    {
+      title: 'Email Campaigns',
+      img: 'EMAIL_CAMPAIGNS'
+    }
+  ]
+};
+
+export const DROP_US_A_LINE = {
+  title: 'Drop us a line',
+  description:
+    '<div class="assertion">Have a specific email <span class="accent-text">task</span> in <br>mind?<br> Let’s discuss the <span class="accent-text">solution.</span></div>',
+  button: { title: 'Contact' },
+  img: 'WORLD' as UiIcon
+};
+
+export const FOOTER = {
+  description:
+    '<div>Focused on email marketing. One <span class="accent-text">task</span> - one <span class="accent-text">solution.</span></div>',
+  input: {
+    type: 'email',
+    name: 'email',
+    error: 'Not correct email',
+    label: 'Subscribe to our newsletter',
+    placeholder: 'Enter your email',
+    icon: 'EMAIL' as UiIcon
+  },
+  button: { title: 'Submit' },
+  socials: {
+    // twitter: 'https://twitter.com/rockpaperscisss',
+    linkedin: {
+      link: 'https://www.linkedin.com/company/rock-paper-scissors-agency/',
+      icon: 'LINKEDIN' as UiIcon,
+      alt: 'Rock Paper Scissors LinkedIn'
+    }
+    // facebook: 'https://www.facebook.com/rockpaperscissorsagency'
+  },
+  copyright: 'Rock Paper Scissors. 2026. All rights reserved'
 };
