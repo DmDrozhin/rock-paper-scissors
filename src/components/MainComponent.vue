2e331b185c9b2972e0b138ad2e5303781483bf14
Твоё имя < email  GitHub>
2026-02-01 23:36:35 +0200

    feat: integrate ModalContactForm into MainComponent for improved user interaction

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -5,18 +5,18 @@
   import SectionHowWeWork from './SectionHowWeWork.vue';
   import SectionDropUsLine from './SectionDropUsLine.vue';
   import Footer from '@/components/AppFooter.vue';
-  import ModalContactForm from '@/components/ModalContactForm.vue';
 </script>
 
 <template>
   <div class="main-component">
-    <Header />
-    <SectionRPS />
-    <SectionWhoWeAre />
-    <SectionHowWeWork />
-    <SectionDropUsLine />
-    <Footer />
-    <ModalContactForm />
+    <Header role="banner" />
+    <main role="main">
+      <SectionRPS />
+      <SectionWhoWeAre />
+      <SectionHowWeWork />
+      <SectionDropUsLine />
+    </main>
+    <Footer role="contentinfo" />
   </div>
 </template>
 
@@ -24,6 +24,7 @@
   .main-component {
     width: 100%;
     background-image: url('@/assets/images/grid-vertical.svg'), url('@/assets/images/grid-horizontal.svg');
+    background-size: contain, contain;
     background-color: var(--color-bg-app);
   }
 </style>
